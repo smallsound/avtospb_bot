8d8b4927957d00f65038603a6eec9907c0e924ec
smallsound <48187395+smallsound@users.noreply.github.com>
2019-03-12 22:11:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,45 +2,43 @@ const TelegramBot = require('node-telegram-bot-api');
 
 const request = require('request');
 
-
 const TOKEN = '696206585:AAF2gp7FHSNLJZ8nbiP3bG2G3kcm1JZNT3g';
 
 
-const bot = new TelegramBot(TOKEN, {polling: true});
+const { InlineKeyboard, ReplyKeyboard, ForceReply } = require("..");
+"use strict";
 
+Object.defineProperty(exports, "__esModule", { value: true });
 
+const __1 = require("..");
+const TelegramBot = require("node-telegram-bot-api");
 
-bot.onText(/\/start/, (msg, match) => {
+if (process.argv.length < 3) {
+	throw new Error("To test this bot, please pass a bot-token to the application.");
+    throw new Error("To test this bot, please pass a bot-token to the application.");
+}
 
-    const chatId = msg.chat.id;
+const token = process.argv[2];
+const bot = new TelegramBot(token, { polling: true });
+let isRKOpen = false;
 
-    bot.sendMessage(chatId, 'Выберите какая валюта вас интересует', {
-        reply_markup: {
-            inline_keyboard: [
-                [
-                    {
-                        text: '€ - EUR',
-                        callback_data: 'EUR'
-                    }, {
-                    text: '$ - USD',
-                    callback_data: 'USD'
-                }, {
-                    text: '₽ - RUR',
-                    callback_data: 'RUR'
-                }, {
-                    text: '₿ - BTC',
-                    callback_data: 'BTC'
-                }
-                ]
-            ]
-        }
-    });
-});
+const rk = new ReplyKeyboard();
+const ik = new InlineKeyboard();
 
-bot.on('callback_query', query => {
-    const id = query.message.chat.id;
+const rk = new __1.ReplyKeyboard();
+const ik = new __1.InlineKeyboard();
+rk
+	.addRow("1:1 button", "1:2 button")
+	.addRow("2:1 button", "2:2 button");
 
-
-})
-
-
+    .addRow("1:1 button", "1:2 button")
+    .addRow("2:1 button", "2:2 button");
+ik
+	.addRow(
+		{ text: "1:1 button", callback_data: "Works!" },
+		{ text: "1:2 button", callback_data: "Works!" }
+	)
+	.addRow(
+		{ text: "2:1 button", callback_data: "Works!" },
+		{ text: "2:2 button", callback_data: "Works!" }
+	);
